5809f259d50516d93f39d1fb111bbefef72f2a70
Raíssa Santos <raixapsantos@gmail.com>
2023-07-02 14:23:25 -0300

    feat: estrutura base

---
+++ filmesporgenero-backend/src/test/java/backend/filmesporgenerobackend/FilmesporgeneroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.filmesporgenerobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmesporgeneroBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
