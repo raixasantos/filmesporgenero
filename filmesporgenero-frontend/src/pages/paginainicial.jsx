3a5d391f19686278d9115e2009f762337ceaf5e3
Neylane <neylanepl@gmail.com>
2023-07-02 21:17:32 -0300

    feat: condição da exibição da tabela

--- filmesporgenero-frontend/src/pages/paginainicial.jsx
+++ filmesporgenero-frontend/src/pages/paginainicial.jsx
@@ -1,37 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const PaginaInicial = () => {
     const [genero, setGenero] = useState('');
     const [filmes, setFilmes] = useState([]);
+    const [isDisabledCard, setDisabledCard] = useState(true);
 
     const handleGeneroChange = (event) => {
         setGenero(event.target.value);
     };
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
+        setDisabledCard(false);
 
-        //listaFilmes 
-
-        //const filmesFiltrados = listaFilmes.filter((filme) => filme.genero === genero);
-
-        //setFilmes(filmesFiltrados);
-    
+        try {
+          const response = await axios.get('http://127.0.0.1:1026/v2/entities?type=Filme&q=genero=='+ genero);
+          setFilmes(response.data);
+        } catch (error) {
+          console.error('Erro ao buscar filmes:', error);
+        }
     };
 
     return (
-        <div className="container" style={{display: "flex", flexDirection: "column", alignItems: "center", padding: "40px 70px"}}>
-          <h2>Informe seu Gênero interesse</h2>
-          <form>
-            <div className="mb-3">
-              <label className="form-label">Gênero:</label>
-              <input className="form-control" type="text" value={genero} onChange={handleGeneroChange}/>
-            </div>
-            <div className="text-center"><button type="button" value="submit" class="btn btn-primary" onClick={(e) => {handleSubmit(e)}}>Buscar Filmes</button></div>
-          </form>
-
-          <h2>Filmes do Gênero: {genero}</h2>
-          <table class="table table-striped">
+        <div className="container" >
+          <div style={{display: "flex", flexDirection: "column", alignItems: "center", padding: "40px 70px"}}> 
+            <h2>Informe seu Gênero de interesse</h2>
+            <form>
+              <div className="mb-3">
+                <label className="form-label">Gênero:</label>
+                <input className="form-control" type="text" value={genero} onChange={(e) => {handleGeneroChange(e); setDisabledCard(true)}}/>
+              </div>
+              <div className="text-center"><button type="button" value="submit" className="btn btn-primary" onClick={(e) => {handleSubmit(e)}}>Buscar Filmes</button></div>
+            </form>
+          </div>
+          {isDisabledCard === false && genero !== ''
+          ?<div style={{display: "flex", flexDirection: "column", alignItems: "center", padding: "40px 70px"}}> 
+            <h2>Filmes do Gênero de interesse</h2>
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">Título</th>
+                  <th scope="col">Gênero</th>
+                  <th scope="col">Canal</th>
+                  <th scope="col">Data/Horário</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filmes.map((filme, index) => (
+                  <tr key={index}>
+                    <td>{filme.titulo}</td>
+                    <td>{filme.genero}</td>
+                    <td>{filme.canal}</td>
+                    <td>{filme.data_horario}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          :<div style={{display: "flex", flexDirection: "column", alignItems: "center", padding: "40px 70px"}}> 
+          <h2>Filmes do Gênero de interesse</h2>
+          <table className="table table-striped">
             <thead>
               <tr>
                 <th scope="col">Título</th>
@@ -40,17 +69,9 @@ const PaginaInicial = () => {
                 <th scope="col">Data/Horário</th>
               </tr>
             </thead>
-            <tbody>
-              {filmes.map((filme, index) => (
-                <tr key={index}>
-                  <td>{filme.titulo}</td>
-                  <td>{filme.genero}</td>
-                  <td>{filme.canal}</td>
-                  <td>{filme.data_horario}</td>
-                </tr>
-              ))}
-            </tbody>
           </table>
+        </div>
+          }
         </div>
       );
 }
